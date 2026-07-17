73c2f2473128204cf6c9968afb92737e4f1385cd
jjlmoya <juanjo1989@gmail.com>
2026-07-17 09:33:04 +0200

    chore: tests and export adaptation

--- src/data.ts
+++ src/data.ts
@@ -1,24 +1,3 @@
-export { pngAJpg } from './tool/pngAJpg';
-export { jpgAPng } from './tool/jpgAPng';
-export { webpAPng } from './tool/webpAPng';
-export { webpAJpg } from './tool/webpAJpg';
-export { pngAWebp } from './tool/pngAWebp';
-export { jpgAWebp } from './tool/jpgAWebp';
-export { svgAPng } from './tool/svgAPng';
-export { svgAJpg } from './tool/svgAJpg';
-export { imagenBase64 } from './tool/imagenBase64';
-export { bmpAJpg } from './tool/bmpAJpg';
-export { bmpAPng } from './tool/bmpAPng';
-export { bmpAWebp } from './tool/bmpAWebp';
-export { avifAJpg } from './tool/avifAJpg';
-export { avifAPng } from './tool/avifAPng';
-export { avifAWebp } from './tool/avifAWebp';
-export { gifAJpg } from './tool/gifAJpg';
-export { gifAPng } from './tool/gifAPng';
-export { gifAWebp } from './tool/gifAWebp';
-export { pngAIco } from './tool/pngAIco';
-export { jpgAIco } from './tool/jpgAIco';
-export { webpAIco } from './tool/webpAIco';
 export { convertersCategory } from './category';
 
 export type { ImageConverterUI } from './shared/ImageConverter.astro';
